c235915b2b08a569991b483073ca5e4ed48fd6d4
andrejkompanec9-bit <andrejkompanec9@gmail.com>
2026-04-02 16:56:35 +0300

    Изменен Hello IT

--- L5/src/Main.java
+++ L5/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello IT");
+        System.out.println("Hello I");
+        System.out.println("Hello Tana");
     }
 }
